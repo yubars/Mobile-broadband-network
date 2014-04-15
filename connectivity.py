5fb1d8efb1090b14deffd5b84c91ef7d9c716ea0
yubars <yubaraj@gmail.com>
2014-04-15 16:39:30 +0200

    create connectivity.py

---
+++ connectivity.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python
+
+import sys
+import os
+import csv
+from math import radians, cos, sin, asin, sqrt
+def usage():
+        print >> sys.stderr, '\n' + str(THISFILENAME)+" [gps file] [ppp file] [new file]" + '\n'
+        sys.exit(0)
+
+#CLIENT:
+if __name__ == '__main__':
+
+        #SIGINT 
+        #signal.signal(signal.SIGINT, signal_handler)
+        if( len(sys.argv) < 2 ):
+                usage()
+        elif( len(sys.argv) == 2 ):
+                        file = sys.argv[1]
+			data = []
+			datafile1 = open(file, 'r')
+			readerg = csv.reader(datafile1)
+			for row in readerg:
+    				data.append(row)
+			heading1 = data.pop(0)
+			mode_c = 0
+			mode_cn = 0
+			mode = 0
+			submode_c = 0
+			submode_cn = 0
+			submode = 0
+			lac_c = 0
+			lac_cn = 0
+			lac = 0
+			cell_c = 0
+			cell_cn = 0
+			cell = 0
+			other_c = 0
+			other = 0
+			conn = 0
+			for d1 in data:
+				conn += 1
+				i1 = data.index(d1)
+				last = len(data)
+				if (i1 < last-1):
+					if((int(data[i1+1][4]) - int(data[i1][4])) > 1):
+						print conn
+						conn = 0
+						if(data[i1][6] != data[i1+1][6]):
+							mode_c += 1
+							mode += (int(data[i1+1][4]) - int(data[i1][4]) -1)
+						elif(data[i1][7] != data[i1+1][7]):
+							submode_c += 1
+							submode += (int(data[i1+1][4]) - int(data[i1][4])-1)
+						elif(data[i1][9] != data[i1+1][9]):
+                                                        lac_c += 1
+                                                        lac += (int(data[i1+1][4]) - int(data[i1][4])-1)
+						elif(data[i1][8] != data[i1+1][8]):
+                                                        cell_c += 1
+                                                        cell += (int(data[i1+1][4]) - int(data[i1][4])-1)
+						else:
+							other_c += 1
+							other += (int(data[i1+1][4]) - int(data[i1][4])-1)			
+					if(data[i1][6] != data[i1+1][6]):
+                                        	mode_cn += 1
+                                        if(data[i1][7] != data[i1+1][7]):
+                                                submode_cn += 1
+                                        if(data[i1][9] != data[i1+1][9]):
+                                                lac_cn += 1
+                                        if(data[i1][8] != data[i1+1][8]):
+                                                cell_cn += 1
+			total = mode+submode+lac+cell+other
+			sent = int(data[last-1][4]) - int(data[0][4]) +1
+			
+			print conn
+			print last
+
+
+
+        else:
+                usage()
